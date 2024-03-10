fab133ce56e05e9b33d545f06114c38bf5836854
Milton Hultgren <milton.hultgren@elastic.co>
2024-03-10 21:13:28 +0100

    Add basic error handling in meals route

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -26,7 +26,8 @@ router.get("/", async (request, response) => {
     const titles = await knex("meal").select("title");
     response.json(titles);
   } catch (error) {
-    throw error;
+    console.error(error);
+    response.status(500).send('HTTP 500: Internal server error');
   }
 });
 
