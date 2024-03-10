0e2fba19446a6110f1a3593e3fdeb6f7bb2b1aee
Milton Hultgren <milton.hultgren@elastic.co>
2024-03-10 22:05:02 +0100

    Read production config from environment variables

--- knexfile.js
+++ knexfile.js
@@ -15,11 +15,11 @@ export const development = {
 export const production = {
   client: 'postgres',
   connection: {
-    host: '',
-    port: 5432,
-    database: '',
-    user: '',
-    password: '',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     ssl: { rejectUnauthorized: false },
   },
   pool: {
